55bf7a4689ea30f486e073e43fe55427ad0b7914
vllddlvh <147725837+vllddlvh@users.noreply.github.com>
2025-01-25 18:58:17 +0700

    p1

---
+++ LearnFE/src/main/java/com/mycompany/LearnFeApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFeApplication.class, args);
+	}
+
+}
